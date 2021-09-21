5d9fe875e106b8655533f99b8f7ba69c87ab4dde
javitocor <javitocor@gmail.com>
2021-09-21 16:28:29 +0700

    add headers cors middleware

--- questionsApp/settings.py
+++ questionsApp/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'psychotests',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
