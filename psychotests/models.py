900ae31359bce33009012cd17ab219612f0cded0
javitocor <javitocor@gmail.com>
2021-09-21 11:13:19 +0700

    implement models

--- psychotests/models.py
+++ psychotests/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+ 
+class Question(models.Model):
+    text = models.CharField(max_length=255, required=True)
+    topic = models.CharField(max_length=255, null=True, blank=True, default=None)
+ 
+class Answer(models.Model):
+    text = models.CharField(max_length=255, required=True)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
