97f4015676bd139be60098423a2c393d0b16da0a
javitocor <javitocor@gmail.com>
2021-09-21 16:15:05 +0700

    configure django to frontend

--- questionsApp/settings.py
+++ questionsApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'questionsApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +127,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'frontend/build/static')
+]
+
+CORS_ORIGIN_WHITELIST = [
+
+    "http://localhost:3000",
+
+]
--- questionsApp/urls.py
+++ questionsApp/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
+\
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('psychotests.urls')),
+    path('', TemplateView.as_view('index.html'))
 ]
