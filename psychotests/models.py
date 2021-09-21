99b0ab4ede3204ca50e3518cd3aa2bfc2b0e6b1e
javitocor <javitocor@gmail.com>
2021-09-21 13:07:56 +0700

    make migrations and migrate

--- psychotests/models.py
+++ psychotests/models.py
@@ -2,10 +2,10 @@ from django.db import models
 # Create your models here.
  
 class Question(models.Model):
-    text = models.CharField(max_length=255, required=True)
+    text = models.CharField(max_length=255)
     topic = models.CharField(max_length=255, null=True, blank=True, default=None)
  
 class Answer(models.Model):
-    text = models.CharField(max_length=255, required=True)
+    text = models.CharField(max_length=255)
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
     value = models.PositiveIntegerField()
--- psychotests/urls.py
+++ psychotests/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from rest_framework import routers
 from . import views
 router = routers.DefaultRouter()
-router.register('questions', views.PostViewSet)
+router.register('questions', views.QuestionViewSet)
  
  
 urlpatterns = [
     path('', include(router.urls)),
-    url(r'questions/(?P<pk>\d+)/answers/$', view=views.PostViewSet.as_view({'get':'answers', 'post':'answers'}), name='question-answers'),
-    url(r'questions/(?P<pk>\d+)/answers/(?P<answer>\d+)/$', view=views.PostViewSet.as_view({'delete':'remove_answer'}), name='delete-answer')
+    url(r'questions/(?P<pk>\d+)/answers/$', view=views.QuestionViewSet.as_view({'get':'answers', 'post':'answers'}), name='question-answers'),
+    url(r'questions/(?P<pk>\d+)/answers/(?P<answer>\d+)/$', view=views.QuestionViewSet.as_view({'delete':'remove_answer'}), name='delete-answer')
 ]
